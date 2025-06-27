efa52cafca3c72809d21e66ad5a521e12600d5c5
Yuzuki <165975846+YzkSK@users.noreply.github.com>
2025-06-27 23:21:11 +0900

    ボーカル分離

---
+++ Backend/separator_test.py
@@ -0,0 +1,39 @@
+from audio_separator.separator import Separator
+from yt_dlp import YoutubeDL
+
+def download_and_separate_audio():
+    url = input("Enter the YouTube video URL: ")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [
+            {'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192'},
+            {'key': 'FFmpegMetadata'},
+        ],
+        'outtmpl': './downloaded_audio/download.%(ext)s',
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    title = ydl.extract_info(url, download=False).get('title', 'downloaded_audio')
+    print(f"Downloaded audio for: {title}")
+
+    input_file = './downloaded_audio/download.wav'
+
+    separator = Separator()
+
+    output_file = {
+        "Vocals": f'(Vocals)_{title}',
+        "Instrumental": f'(Inst)_{title}',
+    }
+
+    separator.load_model()
+    output_file = separator.separate(input_file, output_file)
+
+    return output_file
+
+result = download_and_separate_audio()
+print(result)
