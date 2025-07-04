d10fa8921382e4d61d14beeefa438abd2a35fd26
oxo-saito-oxo <166021913+oxo-saito-oxo@users.noreply.github.com>
2025-07-04 12:18:00 +0900

    ヘッダー調整

--- musicpitchfinder/src/main.tsx
+++ musicpitchfinder/src/main.tsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Wrapper from "./pages/Wrapper";
+import UI_page2 from "./UI_page2";
+import UI_page3 from "./UI_page3";
 import { ChakraProvider ,defaultSystem} from '@chakra-ui/react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'//React Routerのインポート
 
@@ -13,9 +15,10 @@ createRoot(document.getElementById('root')!).render(
     <ChakraProvider value={defaultSystem}>
       <BrowserRouter>
           <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/Login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/Register" element={<Register />} />
+          <Route path="/UI_page2" element={<UI_page2 />} />
+          <Route path="/UI_page3" element={<UI_page3 />} />
           <Route
             path="/dashboard"
             element={
